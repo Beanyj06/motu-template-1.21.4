f04a2d044ed98124a1f2b21b71566571a2cae743
Beanyj06 <branyj06@gmail.com>
2025-02-15 17:43:01 +0000

    0.1

---
+++ src/main/java/com/beanyj/motu/MOTU.java
@@ -0,0 +1,16 @@
+package com.beanyj.motu;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MOTU implements ModInitializer {
+	public static final String MOD_ID = "motu";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/beanyj/motu/MOTUModClient.java
@@ -0,0 +1,10 @@
+package com.beanyj.motu;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MOTUModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
